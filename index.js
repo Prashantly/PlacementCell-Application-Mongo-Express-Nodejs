a3e491852b5a36dc97a54c4d3f0d1c4aaf700e5f
Prashant <prashantyallatti79968@gmail.com>
2023-05-20 13:03:57 +0530

    setup view engine

--- index.js
+++ index.js
@@ -4,6 +4,11 @@ const app = express();
 //use express router
 app.use("/", require("./routes"));
 
+//setup view engine
+app.set("view engine", "ejs");
+app.set("views", "./views");
+
+//listen to port 3000
 const port = process.env.PORT || 3000;
 
 app.listen(port, (err) => {
