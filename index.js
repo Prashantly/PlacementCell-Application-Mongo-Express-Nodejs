9bda5bad84c3211e4a520a4eccb274417ad26851
Prashant <prashantyallatti79968@gmail.com>
2023-05-20 12:25:22 +0530

    setup express router

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const express = require("express");
 const app = express();
 
-const port = 3000;
+//use express router
+app.use("/", require("./routes"));
+
+const port = process.env.PORT || 3000;
 
 app.listen(port, (err) => {
   if (err) throw err;
---
+++ routes/index.js
@@ -0,0 +1,7 @@
+//entry point for all the routes
+const express = require("express");
+const router = express.Router();
+
+console.log("router loaded");
+
+module.exports = router;
