8f8ee7e43c1102207592ca08fd5ddbbfb2c806b3
Prashant <prashantyallatti79968@gmail.com>
2023-05-20 22:07:01 +0530

    setting up static pages

--- index.js
+++ index.js
@@ -3,14 +3,20 @@ const app = express();
 //using layout library
 const expressLayouts = require("express-ejs-layouts");
 
-//use express router
-app.use("/", require("./routes"));
 app.use(expressLayouts);
+
+//extract styles and scripts from sub pages into layouts
+app.set("layout extractStyles", true);
+app.set("layout extractScripts", true);
+
+app.use(express.static("./assets"));
 
 //setup view engine
 app.set("view engine", "ejs");
 app.set("views", "./views");
 
+//use express router
+app.use("/", require("./routes"));
 //listen to port 3000
 const port = process.env.PORT || 3000;
 
