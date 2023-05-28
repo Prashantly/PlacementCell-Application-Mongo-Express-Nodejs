0e31f10d88e451e1554fb6513180c0fd0f1a4d20
Prashant <prashantyallatti79968@gmail.com>
2023-05-28 19:38:07 +0530

    download student data as CSV file

--- routes/index.js
+++ routes/index.js
@@ -2,10 +2,12 @@
 const express = require("express");
 const router = express.Router();
 const homeController = require("../controllers/home_controller");
+const csvController = require("../controllers/csv_controller");
 
 router.get("/", homeController.home);
 router.use("/users", require("./users"));
 router.use("/students", require("./students"));
 router.use("/interviews", require("./interviews"));
+router.get("/download-report", csvController.downloadCSV);
 
 module.exports = router;
