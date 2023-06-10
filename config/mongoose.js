ebc7a222f850c442e6cdcb673f4a66c85705550f
Prashant <prashantyallatti79968@gmail.com>
2023-06-10 10:56:42 +0530

    remove comments and ready for deployment

--- config/mongoose.js
+++ config/mongoose.js
@@ -21,17 +21,3 @@ const connectDB = async () => {
 
 connectDB();
 module.exports = db;
-
-// mongoose.connect("mongodb://127.0.0.1/placement_cell_dev", {
-//   useNewUrlParser: true,
-//   useUnifiedTopology: true,
-// });
-
-// // Equivalent code without bind
-// db.on("error", (error) => {
-//   console.error("Error connecting to MongoDB", error);
-// });
-
-// db.once("open", function () {
-//   console.log("connected to Database : MongoDB");
-// });
